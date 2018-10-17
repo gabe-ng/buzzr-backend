0d3557ca9c3320dd8879635cb71827c2eb748611
Gabriel Ng <gabrielng15@gmail.com>
2018-10-16 20:33:55 -0700

    #9 updated buzz model

--- src/buzzr_app/models.py
+++ src/buzzr_app/models.py
@@ -6,15 +6,16 @@ class Profile(models.Model):
     full_name = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
     email = models.CharFIeld(max_length=100)
-    follwers = models.IntegerField()
-    join_date = models.DateField()
+    follwers = models.IntegerField(default=0)
+    join_date = models.DateTimeField(auto_now=True)
 
 
 class Buzz(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    num_likes = models.IntegerField()
+    create_date = models.DateTimeField(auto_now=True)
+    num_likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
